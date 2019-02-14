ed5708b6efae4eca576af2abe2b1c13b5fafb109
chen_jia_jia <chenjiajia@zuzuche.com>
2019-02-14 17:57:47 +0800

    添加注释

--- src/store.js
+++ src/store.js
@@ -293,6 +293,8 @@ function resetStoreVM(store, state, hot) {
     }
 }
 
+
+// 安装模块
 function installModule(store, rootState, path, module, hot) {
     const isRoot = !path.length
     const namespace = store._modules.getNamespace(path)
